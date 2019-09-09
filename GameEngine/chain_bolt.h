8f43b423517256a3ef9ba5c798524186caa0634e
danjr26 <driehm1@nd.edu>
2019-09-09 16:13:30 -0400

    small additions

--- GameEngine/chain_bolt.h
+++ GameEngine/chain_bolt.h
@@ -1,1 +1,24 @@
-#pragma once
+#ifndef CHAIN_BOLT_H
+#define CHAIN_BOLT_H
+
+#include "weapon.h"
+
+class ChainBolt : public Weapon {
+private:
+	enum class State {
+		off,
+		on
+	} mState;
+	double mAccum;
+	double mReloadTime;
+	double mRecoil;
+
+public:
+	ChainBolt();
+
+	void update(double in_dt, Feedback* out_feedback);
+	void startFire() override;
+	void endFire() override;
+};
+
+#endif
